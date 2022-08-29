1541736fad113c1d9cbebaf0eb44c3b6fe851752
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-08-29 23:15:44 +0700

    Adding DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,66 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $id = \App\Models\Sensor::create([
+            'plant_name'     => 'Panel1',
+            'plant_type'     => 'Panel',
+            'sensor'    => 'bmp280_1',
+        ])->id;
+        \App\Models\SetPoint::create([
+            'sensor_id' => $id,
+            'warning'   => 30,
+            'danger'    => 33
+        ]);
+
+        $id = \App\Models\Sensor::create([
+            'plant_name'     => 'Panel1',
+            'plant_type'     => 'Panel',
+            'sensor'    => 'bmp280_2',
+        ])->id;
+        \App\Models\SetPoint::create([
+            'sensor_id' => $id,
+            'warning'   => 30,
+            'danger'    => 33
+        ]);
+
+        $id = \App\Models\Sensor::create([
+            'plant_name'     => 'Panel1',
+            'plant_type'     => 'Panel',
+            'sensor'    => 'bmp280_3',
+        ])->id;
+        \App\Models\SetPoint::create([
+            'sensor_id' => $id,
+            'warning'   => 30,
+            'danger'    => 33
+        ]);
+
+        \App\Models\Sensor::create([
+            'plant_name'     => 'Motor1',
+            'plant_type'     => 'Motor',
+            'sensor'    => 'adxl',
+        ]);
+
+        $id = \App\Models\Sensor::create([
+            'plant_name'     => 'Motor1',
+            'plant_type'     => 'Motor',
+            'sensor'    => 'mlx',
+        ])->id;
+        \App\Models\SetPoint::create([
+            'sensor_id' => $id,
+            'warning'   => 28,
+            'danger'    => 31
+        ]);
+
+        $id = \App\Models\Sensor::create([
+            'plant_name'     => 'Motor1',
+            'plant_type'     => 'Motor',
+            'sensor'    => 'pzem',
+        ])->id;
+        \App\Models\SetPoint::create([
+            'sensor_id' => $id,
+            'warning'   => 0.09,
+            'danger'    => 0.11
+        ]);
         // \App\Models\User::factory(10)->create();
     }
 }
