f9e3579be57009a1617adf93845cbc97c057a1a3
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-09-09 23:39:27 +0700

    Update Motor Chart

--- resources/views/monitoring/motor.blade.php
+++ resources/views/monitoring/motor.blade.php
@@ -20,6 +20,25 @@
   </div>
   <!--end of Header--> 
 
+  <h6 class="mb-0 text-uppercase">MLX Temperature Sensor Monitoring</h6>
+  <hr>
+  <div class="row justify-content-center">
+    <div class="col-xl-6">
+      <div class="card">
+        <div class="card-body">
+          <div id="temperatureChart"></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-6">
+      <div class="card">
+        <div class="card-body">
+          <div id="ambientChart"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <h6 class="mb-0 text-uppercase">ADXL Vibration Sensor Monitoring</h6>
   <hr>
   <div class="row justify-content-center">
@@ -41,6 +60,25 @@
       <div class="card">
         <div class="card-body">
           <div id="vibrationZChart"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <h6 class="mb-0 text-uppercase">PZEM Volt and Current Sensor Monitoring</h6>
+  <hr>
+  <div class="row justify-content-center">
+    <div class="col-xl-6">
+      <div class="card">
+        <div class="card-body">
+          <div id="voltChart"></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-6">
+      <div class="card">
+        <div class="card-body">
+          <div id="currentChart"></div>
         </div>
       </div>
     </div>
@@ -87,21 +125,74 @@
     }
   }
 
+  var temperatureChart = new ApexCharts(document.getElementById("temperatureChart"), chartOption);
+  temperatureChart.render();
+  var ambientChart = new ApexCharts(document.getElementById("ambientChart"), chartOption);
+  ambientChart.render();
+
   var vibrationXChart = new ApexCharts(document.getElementById("vibrationXChart"), chartOption);
   vibrationXChart.render();
   var vibrationYChart = new ApexCharts(document.getElementById("vibrationYChart"), chartOption);
   vibrationYChart.render();
   var vibrationZChart = new ApexCharts(document.getElementById("vibrationZChart"), chartOption);
   vibrationZChart.render();
+
+  var voltChart = new ApexCharts(document.getElementById("voltChart"), chartOption);
+  voltChart.render();
+  var currentChart = new ApexCharts(document.getElementById("currentChart"), chartOption);
+  currentChart.render();
   
   var updateChart = function () {
+      var dataTemperature = [];
+      var dataAmbient = [];
+
       var dataVibrationX = [];
       var dataVibrationY = [];
       var dataVibrationZ = [];
 
+      var dataVolt = [];
+      var dataCurrent = [];
+
       $.ajax({
         type: "GET",
-        url: url + '/api/motor_sensor/Motor1/vibration',
+        url: url + '/api/Motor1/temperature?filter=30&unit=minutes',
+        dataType: 'JSON',
+        success: function (resp) {
+          resp.data[0].temperature.forEach(data => {
+            let time = moment(data.created_at).format("MMM, DD YYYY - HH:mm:ss");
+            let temperature = {x: time, y: data.temperature};
+            dataTemperature.push(temperature);
+            let ambient = {x: time, y: data.ambient};
+            dataAmbient.push(ambient);  
+          });
+
+          temperatureChart.updateOptions({
+            series: [{
+              name: 'Temperature',
+              data: dataTemperature
+            }],
+            colors: ["#0d6efd"],
+            title: {
+              text: "Temperature Monitoring"
+            }
+          });
+
+          ambientChart.updateOptions({
+            series: [{
+              name: 'Ambient',
+              data: dataAmbient
+            }],
+            colors: ["#dc3545"],
+            title: {
+              text: "Ambient Monitoring"
+            }
+          });
+        }
+      });
+
+      $.ajax({
+        type: "GET",
+        url: url + '/api/Motor1/vibration?filter=30&unit=minutes',
         dataType: 'JSON',
         success: function (resp) {
           resp.data[0].vibration.forEach(data => {
@@ -121,6 +212,7 @@
               name: 'X Axis',
               data: dataVibrationX
             }],
+            colors: ["#0d6efd"],
             title: {
               text: "X Axis Vibration"
             }
@@ -131,6 +223,7 @@
               name: 'Y Axis',
               data: dataVibrationY
             }],
+            colors: ["#dc3545"],
             title: {
               text: "Y Axis Vibration"
             }
@@ -141,8 +234,48 @@
               name: 'Z Axis',
               data: dataVibrationZ
             }],
+            colors: ["#198754"],
             title: {
               text: "Z Axis Vibration"
+            }
+          });
+        }
+      });
+
+      $.ajax({
+        type: "GET",
+        url: url + '/api/Motor1/current?filter=30&unit=minutes',
+        dataType: 'JSON',
+        success: function (resp) {
+          console.log(resp)
+          resp.data[0].current.forEach(data => {
+            let time = moment(data.created_at).format("MMM, DD YYYY - HH:mm:ss");
+            let volt = {x: time, y: data.volt};
+            dataVolt.push(volt);
+
+            let current = {x: time, y: data.ampere};
+            dataCurrent.push(current);
+          });
+
+          voltChart.updateOptions({
+            series: [{
+              name: 'Volt',
+              data: dataVolt
+            }],
+            colors: ["#0d6efd"],
+            title: {
+              text: "Volt Monitoring"
+            }
+          });
+
+          currentChart.updateOptions({
+            series: [{
+              name: 'Current',
+              data: dataCurrent
+            }],
+            colors: ["#dc3545"],
+            title: {
+              text: "Current Monitoring"
             }
           });
         }
